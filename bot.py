501b129eb938a98a8de584e0506eff6bef0aff61
FelixBand <felixband@gmail.com>
2024-07-21 17:09:49 +0200

    use requests instead of aiohttps

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
 import discord
 from discord.ext import commands, tasks
-import aiohttp
-import asyncio
+import requests
 import random
+import time
 
 # Initialize the Discord bot
 intents = discord.Intents.default()
@@ -14,23 +14,22 @@ async def on_ready():
     print(f'Logged in as {bot.user.name}')
     change_status.start()  # Start the task to change status
 
-async def get_random_game_name():
+def get_random_game_name():
     while True:
         # Get a random appid between 1 and 999999
         appid = random.randint(1, 999999)
-        async with aiohttp.ClientSession() as session:
-            url = f'https://store.steampowered.com/api/appdetails?appids={appid}'
-            async with session.get(url) as response:
-                if response.status == 200:
-                    data = await response.json()
-                    if str(appid) in data and data[str(appid)]['success']:
-                        game_name = data[str(appid)]['data']['name']
-                        return game_name
-                await asyncio.sleep(1)  # Avoid hammering the API
+        url = f'https://store.steampowered.com/api/appdetails?appids={appid}'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            if str(appid) in data and data[str(appid)]['success']:
+                game_name = data[str(appid)]['data']['name']
+                return game_name
+        time.sleep(1)  # Avoid hammering the API
 
-@tasks.loop(minutes=30)
+@tasks.loop(hours=1)
 async def change_status():
-    game_name = await get_random_game_name()
+    game_name = get_random_game_name()
     await bot.change_presence(activity=discord.Game(name=game_name))
     print(f'Changed status to: Playing {game_name}')
 
